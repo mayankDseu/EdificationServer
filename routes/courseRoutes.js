601ce9188c7b0624f17d805ed1fa6f1787c0456e
mayank <134508591+mayankDseu@users.noreply.github.com>
2024-07-22 18:45:48 +0530

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -10,24 +10,13 @@ router.route("/courses").get(getAllCourses);
 router.route("/course/:id").get(getCourseLectures);
 
 
-
 //create new course - only admin
 router.route("/createcourse").post(isAuthenticated,authorizeAdmin,singleUpload,createCourse);
 
 
 // add lectures, delete courses, get courses details
 
-<<<<<<< HEAD
-router.route("/course/:id").post(isAuthenticated,authorizeAdmin,singleUpload,addLecture
-=======
-<<<<<<< HEAD
-router.route("/course/:id").get(getCourseLectures).post(
-=======
-router.route("/course/:id").get(isAuthenticated,authorizeAdmin,getCourseLectures).post(
->>>>>>> b5676d0d3ab91507bd2e2dab6be94aadaa53b8aa
-    isAuthenticated,authorizeAdmin,singleUpload,addLecture
->>>>>>> 498faeb8209dae336a3782a7e7e2d5a75741ebf0
-).delete(isAuthenticated,authorizeAdmin,deleteCourse);
+router.route("/course/:id").post(isAuthenticated,authorizeAdmin,singleUpload,addLecture).delete(isAuthenticated,authorizeAdmin,deleteCourse);
 
 //delete lecture
 
