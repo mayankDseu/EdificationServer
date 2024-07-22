b7c2647b6158de48d73e43d216d16e3660d6fa15
mayank <134508591+mayankDseu@users.noreply.github.com>
2024-07-22 15:12:02 +0530

    v1

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {deleteLecture,deleteCourse, getAllCourses,getCourseLectures,addLecture } from "../controllers/courseController.js";
+import {deleteLecture,deleteCourse, getAllCourses,getCourseLectures,addLecture, editLecture } from "../controllers/courseController.js";
 import {createCourse} from "../controllers/courseController.js";
 import singleUpload from "../middlewares/multer.js";
 import { isAuthenticated,authorizeAdmin,authorizeSubscribers } from "../middlewares/auth.js";
@@ -9,18 +9,21 @@ const router = express.Router();
 router.route("/courses").get(getAllCourses);
 
 
+
 //create new course - only admin
 router.route("/createcourse").post(isAuthenticated,authorizeAdmin,singleUpload,createCourse);
 
 
 // add lectures, delete courses, get courses details
 
-router.route("/course/:id").get(isAuthenticated,authorizeSubscribers,getCourseLectures).post(
+router.route("/course/:id").get(isAuthenticated,authorizeAdmin,getCourseLectures).post(
     isAuthenticated,authorizeAdmin,singleUpload,addLecture
 ).delete(isAuthenticated,authorizeAdmin,deleteCourse);
 
 //delete lecture
 
-router.route("/lecture").delete(isAuthenticated,authorizeAdmin,deleteLecture)
+router.route("/lecture").delete(isAuthenticated,authorizeAdmin,deleteLecture).put(editLecture);
+
+
 
 export default router;
